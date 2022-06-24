772d87781f7558220409c95f7d6404f2954f2c3a
P3d11 <pee.marchi@hotmail.com>
2022-06-24 16:37:46 -0300

    update something

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+from multiprocessing.spawn import prepare
 from tabnanny import verbose
 import tkinter as tk
 from tkinter import simpledialog
 import cv2 as cv
-from keras.models import Sequential, save_model, load_model
+from keras.models import load_model
 import numpy as np
 import os
 import PIL.Image, PIL.ImageTk
 from cv2 import WND_PROP_VISIBLE
+
 
 class App:
 
@@ -14,25 +16,26 @@ class App:
 
         cam = cv.VideoCapture(0)
         
-        IMG_SIZE = 38
-        pred = []
+        CATEGORIES = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
+
         classif = cv.CascadeClassifier('other_data/haarcascade_frontalface_default.xml')
         filepath = './saved_model'
         model = load_model(filepath, compile = True)
                 
-        i = 0
         while True:
-            i+=1
             
+            #take frame
             ref, frame = cam.read()
-            
             cv.imshow('cam', frame)
             
             key = cv.waitKey(1)
             
+            #close window
             if cv.getWindowProperty('cam', WND_PROP_VISIBLE) == 0:
                 break
-            
+        
+                    
+            #change 
             gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
             faces = classif.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
             
@@ -42,81 +45,20 @@ class App:
                 
                 if len(faces) != 0:
                     
-                    pred = np.array(pred, face)
-                    pred = np.array(pred).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-
-                    predicts = model.predict(x=pred, batch_size=10, verbose=0)
-                    
-                    for aux in predicts:
-                        print(aux)
-
-                    
-                    """ prediction_img = []
-                    prediction_img[0].reshape((48, 48))
-                    
-                    sample_to_predict = []
-                    sample_to_predict.append(prediction_img[0]) """
-                    
-                    
-                    
-                    print(i)
-                else:
-                    print(-i)
-                
-                if face.shape[0] >= 200 and face.shape[1] >= 200:
-                    face = cv.resize(face, (48, 48))
-                    
-            
-
-            
+                    prediction = model.predict([prepare('frame.jpg')])
+                    print(prediction)   
             
         cam.release()
         
         
+    def prepare(filepath):
 
-
-"""     def update(self):
-        if self.auto_predict:
-            print(self.predict())
-
-        ret, frame = self.camera.get_frame()
-
-        if ret:
-            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-
-        self.window.after(self.delay, self.update) """
-
-"""     def predict(self):
-        CATEGORIES = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
-
-        frame = self.camera.get_frame()
-        prediction = self.model.predict(frame)
-
-        if prediction == 1:
-            self.class_label.config(text=CATEGORIES[0])
-            return CATEGORIES[0]
+        IMG_SIZE = 38
         
-        if prediction == 2:
-            self.class_label.config(text=CATEGORIES[1])
-            return CATEGORIES[1]
+        img_array = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
+        img_array = img_array.resize(IMG_SIZE, IMG_SIZE)
         
-        if prediction == 3:
-            self.class_label.config(text=CATEGORIES[2])
-            return CATEGORIES[2]
+        new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
         
-        if prediction == 4:
-            self.class_label.config(text=CATEGORIES[3])
-            return CATEGORIES[3]
-        
-        if prediction == 5:
-            self.class_label.config(text=CATEGORIES[4])
-            return CATEGORIES[4]
-        
-        if prediction == 6:
-            self.class_label.config(text=CATEGORIES[5])
-            return CATEGORIES[5]
-        
-        if prediction == 7:
-            self.class_label.config(text=CATEGORIES[6])
-            return CATEGORIES[6] """
+        return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)            
+    
--- data.py
+++ data.py
@@ -1,12 +1,3 @@
-""" import tensorflow as tf
-from tensorflow import keras
-from keras import layers
-import matplotlib.pyplot as plt
-
-import numpy as np
-import train
-import cv2 as cv
-import os """
 from ast import dump
 from cProfile import label
 from dbm import dumb
@@ -20,77 +11,60 @@ import glob
 import random
 import pickle
 
-DATADIR = 'train/'
-CATEGORIES = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
-IMG_SIZE = 48
-        
-training_data = []
-train = np.array([])
-test = np.array([])
+class Data:
     
-for category in CATEGORIES:
-    path = glob.glob(DATADIR + category + '/*.png')
-    class_num = CATEGORIES.index(category)
-    #print(path)
-            
-    for img in path:
-        try:
-            img_array = cv.imread(img, cv.IMREAD_GRAYSCALE)
-            new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
-            training_data.append([new_array, class_num])
-            #print(os.path.join(path, img))
-        except Exception as e:
-            pass
+    def __init__(self):
+        
+        DATADIR = 'train/'
+        CATEGORIES = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
+        IMG_SIZE = 48
                 
-#print(len(training_data))
-
-random.shuffle(training_data)
-
-print(len(training_data))
-        
-for sample in training_data[:10]:
-    print(sample[1])
-    
-cont = 0
-
-for features, label in training_data:  
-    print(cont)
-    cont += 1
-    train = np.append(train, features)
-    test = np.append(test, label) # arrunasr
+        training_data = []
+        train = np.array([])
+        test = np.array([])
             
-    train = np.array(train).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-    
-print(train.shape)
-print(test.shape)
-            
-pickle_out = open("train.pickle", "wb")
-pickle.dump(train, pickle_out)
-pickle_out.close()
-
-
-pickle_out = open("test.pickle", "wb")
-pickle.dump(test, pickle_out)
-pickle_out.close()
-            
-        
-"""         classif = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
-        x = []
-
-        image_size = (48, 48)
-        batch_size = 32
-
-        for name in classif:
-            
-            data = []
-            paths = 
-            print(name)
-            for path in paths:
-                if path.endswith('.png'):
-                    image = cv.imread (path)
-                    data.append (image) 
+        for category in CATEGORIES:
+            path = glob.glob(DATADIR + category + '/*.png')
+            class_num = CATEGORIES.index(category)
+            #print(path)
                     
-            x = [name , np.array(data)]
+            for img in path:
+                try:
+                    img_array = cv.imread(img, cv.IMREAD_GRAYSCALE)
+                    new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                    training_data.append([new_array, class_num])
+                    #print(os.path.join(path, img))
+                except Exception as e:
+                    pass
+                        
+        #print(len(training_data))
+
+        random.shuffle(training_data)
+
+        print(len(training_data))
+                
+        for sample in training_data[:10]:
+            print(sample[1])
+            
+        cont = 0
+
+        for features, label in training_data:  
+            print(cont)
+            cont += 1
+            train = np.append(train, features)
+            test = np.append(test, label) # arrunasr
+                    
+            train = np.array(train).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+            
+        print(train.shape)
+        print(test.shape)
+                    
+        pickle_out = open("train.pickle", "wb")
+        pickle.dump(train, pickle_out)
+        pickle_out.close()
 
 
-        x = np.array(x, dtype=2) """
+        pickle_out = open("test.pickle", "wb")
+        pickle.dump(test, pickle_out)
+        pickle_out.close()
+            
