882a30aec5053eb6782690b3b3bf16808cef1185
P3d11 <pee.marchi@hotmail.com>
2022-06-26 15:58:24 -0300

    finished data.py

--- data.py
+++ data.py
@@ -25,23 +25,22 @@ class Data:
     IMG_SIZE = 224
         
     aux_emo = 0
-    aux_already_gone = 2
-                
+    aux_already_gone = 0
 
         
-    while aux_emo == 0:
+    while aux_emo <= 6:
         #print(CATEGORIES[aux_emo])
                 
         aux_secemo = aux_already_gone
             
-        while aux_secemo == 2:
+        while aux_secemo <= 6:
             if(aux_secemo != aux_emo):
                 
                 print(CATEGORIES[aux_emo] + ' and ' + CATEGORIES[aux_secemo])
                 
                 training_data = []
-                train = np.array([])
-                test = np.array([]) 
+                init_train = [] #np.array([])
+                init_test = [] #np.array([]) 
                 
                 path = glob.glob(DATADIR + CATEGORIES[aux_emo] + '/*.png')
                 class_num = CATEGORIES.index(CATEGORIES[aux_emo])
@@ -51,7 +50,7 @@ class Data:
                                 
                 for img in path:
                     try:
-                        img_array = cv.imread(img, cv.IMREAD_GRAYSCALE)
+                        img_array = cv.imread(img)#, cv.COLOR_BGR2RGB)
                         new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
                         training_data.append([new_array, class_num])
                         #print(os.path.join(path, img))
@@ -60,7 +59,7 @@ class Data:
                     
                 for img in path_sec:
                     try:
-                        img_array = cv.imread(img, cv.IMREAD_GRAYSCALE)
+                        img_array = cv.imread(img)#, cv.COLOR_BGR2RGB)
                         new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
                         training_data.append([new_array, class_num_sec])
                         #print(os.path.join(path, img))
@@ -70,26 +69,27 @@ class Data:
                 #print(len(training_data))
 
                 random.shuffle(training_data)
-
-                #print(len(training_data))
+                
+                print(len(training_data))
                         
-                #for sample in training_data[:10]:
-                    #print(sample[1])
+                for sample in training_data[:10]:
+                    print(sample[1])
                     
                 cont = 0
 
                 for features, label in training_data:  
                     print(str(cont) + '/' + str(len(training_data)))
                     cont += 1
-                    train = np.append(train, features)
-                    test = np.append(test, label) # arrunasr
+                    init_train.append(features)
+                    init_test.append(label) # arrunasr
                             
-                    train = np.array(train).reshape(-1, IMG_SIZE, IMG_SIZE, 1)           
+                    train = np.array(init_train).reshape(-1, IMG_SIZE, IMG_SIZE, 3)           
                 
-                #print(train.shape)
-                #print(test.shape)
+                print(train.shape)
+                #print(np.array(test).shape)
                 
                 train = train/255.0
+                test = np.array(init_test)
                 
                 model = MobileNetV2()
                 
@@ -106,28 +106,8 @@ class Data:
                 
                 new_model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
                 
-                new_model.fit(train, test, epochs = 25)
+                new_model.fit(train, test, epochs = 15)
                     
-                """ model = Sequential()
-                    
-                model.add(Conv2D(64, (3,3), input_shape = train.shape[1:]))
-                model.add(Activation('relu'))
-                model.add(MaxPooling2D(pool_size=(2,2)))
-                    
-                model.add(Conv2D(64, (3,3)))
-                model.add(Activation('relu'))
-                model.add(MaxPooling2D(pool_size=(2,2)))
-                    
-                model.add(Flatten())
-                model.add(Dense(64))
-                    
-                model.add(Dense(1))
-                model.add(Activation('sigmoid'))
-                    
-                model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-                    
-                model.fit(train, test, epochs=5, batch_size=32, validation_split=0.1) """
-
                 filepath = './saved_model/' + CATEGORIES[aux_emo] + '_' + CATEGORIES[aux_secemo]
                 save_model(model, filepath)
                 
@@ -136,49 +116,3 @@ class Data:
         aux_already_gone += 1
 
         aux_emo += 1
-            
-        """ for category in CATEGORIES:
-            path = glob.glob(DATADIR + category + '/*.png')
-            class_num = CATEGORIES.index(category)
-            #print(path)
-                    
-            for img in path:
-                try:
-                    img_array = cv.imread(img, cv.IMREAD_GRAYSCALE)
-                    new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
-                    training_data.append([new_array, class_num])
-                    #print(os.path.join(path, img))
-                except Exception as e:
-                    pass
-                        
-        #print(len(training_data))
-
-        random.shuffle(training_data)
-
-        print(len(training_data))
-                
-        for sample in training_data[:10]:
-            print(sample[1])
-            
-        cont = 0
-
-        for features, label in training_data:  
-            print(cont + '/' + len(training_data))
-            cont += 1
-            train = np.append(train, features)
-            test = np.append(test, label) # arrunasr
-                    
-            train = np.array(train).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-            
-        print(train.shape)
-        print(test.shape)
-                    
-        pickle_out = open("train.pickle", "wb")
-        pickle.dump(train, pickle_out)
-        pickle_out.close()
-
-
-        pickle_out = open("test.pickle", "wb")
-        pickle.dump(test, pickle_out)
-        pickle_out.close() """
-            
