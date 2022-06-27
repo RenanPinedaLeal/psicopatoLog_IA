9e30fea23831e6e69c32f0a9850c89eeab52e0a9
P3d11 <pee.marchi@hotmail.com>
2022-06-27 17:14:37 -0300

    now with camera :)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from multiprocessing.spawn import prepare
-from tabnanny import verbose
+from tabnanny import check, verbose
 import tkinter as tk
 from tkinter import simpledialog
 from turtle import shape
@@ -18,29 +18,50 @@ import cv2 as cv
 
 
 class App:
+    
+    IMG_SIZE = 244
+    CATEGORIES = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
+    face_finder = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    aux_emo = 0
+    aux_secemo = 1
+    filepath = './saved_model/' + CATEGORIES[aux_emo] + '_' + CATEGORIES[aux_secemo] + '.h5'
+        
+    #print(filepath)
+        
+    model = load_model(filepath) #load_model(filepath + CATEGORIES[aux_emo] + '_' + CATEGORIES[aux_secemo] + '/', compile = True)
+        
 
     def __init__(self):      
 
         cam = cv.VideoCapture(0)
+
+        i = 0
+        while True:
+            i+=1
+
+            #take frame
+            ref, frame = cam.read()
+
+            cv.imshow('cam', frame)
+            
+            self.pred(frame= frame)
+
+            key = cv.waitKey(1)
+
+            #close window
+            if cv.getWindowProperty('cam', WND_PROP_VISIBLE) == 0:
+                break
         
-        IMG_SIZE = 244
-        CATEGORIES = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
-        face_finder = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        aux_emo = 0
-        aux_secemo = 1
-        filepath = './saved_model/' + CATEGORIES[aux_emo] + '_' + CATEGORIES[aux_secemo] + '.h5'
-        
-        #print(filepath)
-        
-        model = load_model(filepath) #load_model(filepath + CATEGORIES[aux_emo] + '_' + CATEGORIES[aux_secemo] + '/', compile = True)
-        
+                
+    def pred(self, frame ):
+
         frame = cv.imread("frame2.jpg")
         #print(frame.shape)
         
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         #print(gray.shape)
 
-        faces = face_finder.detectMultiScale(gray, 1.1, 4)
+        faces = self.face_finder.detectMultiScale(gray, 1.1, 4)
         
         for x,y,w,h in faces:
             roi_gray = gray[y: y+h, x: x+w]
@@ -52,11 +73,12 @@ class App:
         if(len(roi_img) == 0):
             return
         
-        final_img = cv.resize(roi_img, (224,224))
+        
+        final_img = cv.resize(roi_img, (224, 224))
         final_img = np.expand_dims(final_img, axis=0)
         
         final_img = final_img/255.0
         
-        pred = model.predict(final_img)
+        pred = self.model.predict(final_img)
         
         print(np.argmax(pred))
