cef05e08ff1b0d468887d108899d95cc5362bc32
P3d11 <pee.marchi@hotmail.com>
2022-06-27 16:55:52 -0300

    fix errors

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from keras.models import load_model
 
 import os
 import PIL.Image, PIL.ImageTk
-from cv2 import WND_PROP_VISIBLE
+from cv2 import WND_PROP_VISIBLE, cvtColor
 
 import numpy as np
 import tensorflow as tf
@@ -30,102 +30,33 @@ class App:
         aux_secemo = 1
         filepath = './saved_model/' + CATEGORIES[aux_emo] + '_' + CATEGORIES[aux_secemo] + '.h5'
         
-        print(filepath)
+        #print(filepath)
         
         model = load_model(filepath) #load_model(filepath + CATEGORIES[aux_emo] + '_' + CATEGORIES[aux_secemo] + '/', compile = True)
         
-        frame = cv.imread('frame.png')
+        frame = cv.imread("frame2.jpg")
         #print(frame.shape)
         
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         #print(gray.shape)
-            
+
         faces = face_finder.detectMultiScale(gray, 1.1, 4)
-        #print(faces.shape)
         
         for x,y,w,h in faces:
-            face = np.empty((1, 244, 244, 3))
-            face[0] = np.array(gray).reshape(-1, IMG_SIZE, IMG_SIZE, 3)  
-            face = preprocess_input(face)
+            roi_gray = gray[y: y+h, x: x+w]
+            #print(roi_gray.shape)
+            
+            roi_img = cv.cvtColor(roi_gray, cv.COLOR_BGR2RGB)
+            #print(roi_img.shape)
 
-            if len(faces) != 0:
-                
-                """ data = np.empty((1, 244, 244, 3))
-                data[0] = face
-                data = preprocess_input(data) """
-                """ mid_img = cv.resize(face, (244,244))
-                final_img = np.array(final_img).reshape(-1, IMG_SIZE, IMG_SIZE, 3)          
-                final_img = np.expand_dims(final_img, axis=0)
-                final_img = final_img/255.0 """
-                
-                
-                pred = model.predict(face)
-                print(np.argmax(pred))
-                
-            #fin_face = gray[y:y+h, x:x+w]
-            #print(fin_gray.shape)
-            
-            #fin_color = frame[y:y+h, x:x+w]
-            
-            #cv.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
-            
-            #mid_face = face_finder.detectMultiScale(fin_gray)
-            #print(mid_face[0])
-            
-            """print(len(mid_face))
-            
-            if len(mid_face) == 0:
-                print('--face not detected--')
-                return
-            else:  """
-                           
-            """ for ex,ey,ew,eh in mid_face:
-                fin_face = fin_color[ey:ey+eh, ex:ex+ew] """
-                #print(fin_face[0])
+        if(len(roi_img) == 0):
+            return
         
+        final_img = cv.resize(roi_img, (224,224))
+        final_img = np.expand_dims(final_img, axis=0)
         
+        final_img = final_img/255.0
         
-        #print(final_img)
+        pred = model.predict(final_img)
         
-
-            
-        """ while True:
-            
-            #take frame
-            ref, frame = cam.read()
-            cv.imshow('cam', frame)
-            
-            key = cv.waitKey(1)
-            
-            #close window
-            if cv.getWindowProperty('cam', WND_PROP_VISIBLE) == 0:
-                break
-        
-                    
-            #change 
-            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            faces = classif.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
-            
-            
-            for x,y,w,h in faces:
-                face = gray[y: y+h, x: x+w]
-                
-                if len(faces) != 0:
-                    
-                    prediction = model.predict([prepare('frame.jpg')])
-                    print(prediction)   
-            
-        cam.release()
-        
-        
-    def prepare(filepath):
-
-        IMG_SIZE = 38
-        
-        img_array = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
-        img_array = img_array.resize(IMG_SIZE, IMG_SIZE)
-        
-        new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
-        
-        return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)         """    
-    
+        print(np.argmax(pred))
